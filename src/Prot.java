b85361370e0406c98aa34dd8e171363029b86d1c
delakrya <delakrya2006@yandex.ru>
2015-11-13 00:37:03 +0500

    pop

---
+++ src/Prot.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class Prot {public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int integer = 42;
+
+
+    System.out.println(integer);
+}}
